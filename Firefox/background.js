62c00d173d414c3171098be5ca0598a3cdb9b282
Andrew <76942911+alexfeed1990@users.noreply.github.com>
2022-01-19 21:28:35 +0200

    uhh what

--- Firefox/background.js
+++
@@ -1,8 +0,0 @@
-//Credits to arti
-const filter = {urls: ["<all_urls>"]};
-browser.webRequest.onHeadersReceived.addListener(main, filter, ['responseHeaders', 'blocking']);
-
-function main(details) {
-    let newHeaders = details.responseHeaders.filter(header => header.name != "content-disposition");
-    return {responseHeaders: newHeaders};
-}
